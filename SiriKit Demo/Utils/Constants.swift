aa6fc78668b9931893c785aef0e0a42ade143db6
Andres Rojas <afro17@gmail.com>
2020-01-21 21:49:04 -0500

    Add SiriKit integration

---
+++ SiriKit Demo/Utils/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  SiriKit Demo
+//
+//  Created by Andres Rojas on 21/01/20.
+//  Copyright © 2020 Andres Rojas. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    static let kNewTaskActivity = "com.andres.newTask"
+}
--- SiriKit Demo/ViewModel/TasksViewModel.swift
+++ SiriKit Demo/ViewModel/TasksViewModel.swift
@@ -9,32 +9,36 @@
 import Foundation
 import Combine
 import SwiftUI
+import CoreSpotlight
+import MobileCoreServices
+import Intents
 
 class TasksViewModel: ObservableObject  {
 
     // MARK: - Properties
-    @Published var tasks: [Task]
-    let dataProvider: DataProvider = DataProvider()
+    @Published var tasks: [Task] {
+        didSet {
+            let data = try? JSONEncoder().encode(tasks)
+            UserDefaults.standard.set(data, forKey: "Tasks")
+        }
+    }
+    private let dataProvider: DataProvider = DataProvider()
     var draftTitle: String = ""
     @Published var isEditing: Bool = false
 
     //MARK: -  Init
     init() {
-        tasks  = DataProvider().getTasks()
-
-        let cancelable = $tasks.sink(receiveValue: {
-            let data = try? JSONEncoder().encode($0)
-            UserDefaults.standard.set(data, forKey: "Tasks")
-        })
+        tasks  = dataProvider.getTasks()
     }
 
     // MARK: - Helpers
 
     func createTask() {
-        print("title \(draftTitle)")
         let newTask = Task(title: self.draftTitle, isDone: false)
         self.tasks.insert(newTask, at: 0)
         self.draftTitle = ""
+
+        newTaskShortcut()
     }
 
     func toggleDone(task: Task) {
@@ -47,6 +51,38 @@ class TasksViewModel: ObservableObject  {
         self.tasks.removeAll(where: { $0.id == task.id })
         if self.tasks.isEmpty {
             self.isEditing = false
+        }
+    }
+
+    private func newTaskShortcut() {
+//        let activity = NSUserActivity(activityType: Constants.kNewTaskActivity)
+//        activity.persistentIdentifier = NSUserActivityPersistentIdentifier(Constants.kNewTaskActivity)
+//        activity.isEligibleForSearch = true
+//        activity.isEligibleForPrediction = true
+//
+//        let attributes = CSSearchableItemAttributeSet(itemContentType: kUTTypeItem as String)
+//
+//        activity.title = "Add new task"
+//        attributes.contentDescription = "Let's add you new tasks"
+//
+//        activity.suggestedInvocationPhrase = "One more task!"
+//
+//        activity.contentAttributeSet = attributes
+//
+//        activity.becomeCurrent()
+
+        let intent = AddTaskIntent()
+        intent.suggestedInvocationPhrase = "One more task!"
+        intent.title = "title"
+
+        let interaction = INInteraction(intent: intent, response: nil)
+
+        interaction.donate { error in
+            if let error = error {
+                print("Donation failed \(error.localizedDescription)")
+            } else {
+                print("Donation success")
+            }
         }
     }
 }
